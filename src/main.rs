cc0bf6a77583b3ac27f2fb2ac3f4078a365d38aa
Sean Anderson <sanderson@foreground.co>
2023-02-25 01:48:14 +0000

    hello, world

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use bevy::prelude::*;
+use console_error_panic_hook::set_once as set_panic_hook;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(
+    inline_js = "export function snippetTest() { console.log('Hello from JS FFI 1!'); }"
+)]
+extern "C" {
+    fn snippetTest();
+}
+
+fn main() {
+    set_panic_hook();
+    snippetTest();
+
+    bevy_hello_world();
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn bevy_hello_world() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            web_sys::console::log_2(&"hello".into(), &(&name.0).into());
+        }
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_startup_system(add_people)
+            .add_system(greet_people);
+    }
+}
