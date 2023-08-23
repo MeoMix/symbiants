5c4a88e34265227934a7ac1cdf0225cbdbbe0b06
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2023-08-23 17:06:42 +0000

    bugfix: math was wrong on fast forward time, whoops

--- src/time.rs
+++ src/time.rs
@@ -68,11 +68,14 @@ pub fn play_time(
     mut pending_ticks: ResMut<PendingTicks>,
 ) {
     if pending_ticks.0 > 0 {
+        // Continue fast-forwarding
         pending_ticks.0 -= 1;
     } else if is_fast_forwarding.0 {
+        // Stop fast-forwarding
         fixed_time.period = Duration::from_secs_f32(DEFAULT_TICK_RATE);
         is_fast_forwarding.0 = false;
     } else {
+        // Start fast-forwarding
         // If fixed_time.accumulated() is large (more than a few ms) then it's assumed that either the app was closed
         // or the tab the app runs on was hidden. In either case, need to fast-forward time when app is active again.
         let accumulated_time = fixed_time.accumulated();
@@ -82,11 +85,10 @@ pub fn play_time(
             // The UI becomes unresponsive because the FixedUpdate schedule, when behind, will run in a loop without yielding until it catches up.
             fixed_time.period = accumulated_time;
             let _ = fixed_time.expend();
-
             fixed_time.period = Duration::from_secs_f32(FAST_FORWARD_TICK_RATE);
+
             is_fast_forwarding.0 = true;
-            pending_ticks.0 =
-                ((60.0 * DEFAULT_TICK_RATE) * accumulated_time.as_secs() as f32) as isize;
+            pending_ticks.0 = ((1.0 / DEFAULT_TICK_RATE) * accumulated_time.as_secs() as f32) as isize;
         }
     }
 }
