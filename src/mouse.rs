79f87ce1b903e1feae924bb5741942d867992400
Sean Anderson <Sean.Palmer.Anderson@gmail.com>
2023-08-28 16:18:56 +0000

    Slight refactor of mouse.rs

--- src/mouse.rs
+++ src/mouse.rs
@@ -17,77 +17,68 @@ pub fn handle_mouse_clicks(
     mut food_count: ResMut<FoodCount>,
     is_pointer_captured: Res<IsPointerCaptured>,
 ) {
-    let Ok(window) = primary_window_query.get_single() else {
+    if is_pointer_captured.0 {
         return;
+    }
+
+    let window = match primary_window_query.get_single() {
+        Ok(window) => window,
+        Err(_) => return,
     };
 
+    let cursor_position = match window.cursor_position() {
+        Some(position) => position,
+        None => return,
+    };
+
+    let (camera, camera_transform) = query.single_mut();
+
+    let world_position = camera
+        .viewport_to_world_2d(camera_transform, cursor_position)
+        .unwrap();
+
+    let grid_position = world_to_grid_position(&world_map, world_position);
+
     if mouse_input.just_pressed(MouseButton::Left) {
-        if is_pointer_captured.0 {
-            return;
-        }
-
-        let (camera, camera_transform) = query.single_mut();
-
-        let cursor_position = match window.cursor_position() {
-            Some(position) => position,
-            None => return,
-        };
-
-        let world_position = camera
-            .viewport_to_world_2d(camera_transform, cursor_position)
-            .unwrap();
-
-        // Convert from world to local position
-        let x = world_position.x + (*world_map.width() as f32 / 2.0) - 0.5;
-        let y = -world_position.y + (*world_map.height() as f32 / 2.0) - 0.5;
-
-        // TODO: is abs + round the right way to do this? maybe round() will off-by-one?
-        let grid_position = Position {
-            x: x.abs().round() as isize,
-            y: y.abs().round() as isize
-        };
-
-        if world_map.is_element(&elements_query, grid_position, Element::Air) {
-            if food_count.0 > 0 {
-                let Some(entity) = world_map.get_element(grid_position) else {
-                    return;
-                };
-                commands.replace_element(grid_position, Element::Food, *entity);
-
-                food_count.0 -= 1;
-            }
-        }
+        handle_left_click(&elements_query, &world_map, &mut commands, &mut food_count, grid_position);
     }
 
     if mouse_input.just_pressed(MouseButton::Right) {
-        if is_pointer_captured.0 {
-            return;
+        handle_right_click(&world_map, &mut commands, grid_position);
+    }
+}
+
+fn world_to_grid_position(world_map: &WorldMap, world_position: Vec2) -> Position {
+    let x = world_position.x + (*world_map.width() as f32 / 2.0) - 0.5;
+    let y = -world_position.y + (*world_map.height() as f32 / 2.0) - 0.5;
+
+    Position {
+        x: x.abs().round() as isize,
+        y: y.abs().round() as isize
+    }
+}
+
+fn handle_left_click(
+    elements_query: &Query<&Element>,
+    world_map: &Res<WorldMap>,
+    commands: &mut Commands,
+    food_count: &mut ResMut<FoodCount>,
+    grid_position: Position,
+) {
+    if world_map.is_element(elements_query, grid_position, Element::Air) && food_count.0 > 0 {
+        if let Some(entity) = world_map.get_element(grid_position) {
+            commands.replace_element(grid_position, Element::Food, *entity);
+            food_count.0 -= 1;
         }
+    }
+}
 
-        let (camera, camera_transform) = query.single_mut();
-
-        let cursor_position = match window.cursor_position() {
-            Some(position) => position,
-            None => return,
-        };
-
-        let world_position = camera
-            .viewport_to_world_2d(camera_transform, cursor_position)
-            .unwrap();
-
-        // Convert from world to local position
-        let x = world_position.x + (*world_map.width() as f32 / 2.0) - 0.5;
-        let y = -world_position.y + (*world_map.height() as f32 / 2.0) - 0.5;
-
-        // TODO: is abs + round the right way to do this? maybe round() will off-by-one?
-        let grid_position = Position {
-            x: x.abs().round() as isize,
-            y: y.abs().round() as isize
-        };
-
-        let Some(entity) = world_map.get_element(grid_position) else {
-            return;
-        };
+fn handle_right_click(
+    world_map: &Res<WorldMap>,
+    commands: &mut Commands,
+    grid_position: Position,
+) {
+    if let Some(entity) = world_map.get_element(grid_position) {
         commands.replace_element(grid_position, Element::Air, *entity);
     }
 }
