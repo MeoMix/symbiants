19831f888d1f712ac757d5590fd93a2c3b21878e
Sean Anderson <sanderson@foreground.co>
2023-10-19 03:06:46 +0000

    Fix

--- src/story_time.rs
+++ src/story_time.rs
@@ -74,7 +74,7 @@ impl StoryElapsedTicks {
         let offset = if self.is_real_time {
             seconds_into_day() as isize
         } else {
-            0 * SECONDS_PER_HOUR
+            6 * SECONDS_PER_HOUR
         };
 
         let seconds_total =
