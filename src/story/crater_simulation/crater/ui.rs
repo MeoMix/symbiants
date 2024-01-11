46cf0351b36f49c7f15a50e38c010c8b1041d72c
Sean Anderson <sanderson@foreground.co>
2024-01-11 20:04:33 +0000

    refactor: update nest model/view references

--- src/story/crater_simulation/crater/ui.rs
+++ src/story/crater_simulation/crater/ui.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use crate::story::grid::VisibleGrid;
+use crate::story::{grid::VisibleGrid, nest_simulation::ModelViewEntityMap};
 
 use super::{AtCrater, Crater};
 
@@ -16,20 +16,26 @@ pub fn on_added_at_crater(
     };
 
     for at_crater_entity in at_crater_query.iter() {
+        // TODO: Stop attaching VisibleGrid to Crater - it's a view concern not a model concern.
         commands.entity(at_crater_entity).insert(visibility);
     }
 }
 
 pub fn on_added_crater_visible_grid(
     crater_query: Query<&Crater, Added<VisibleGrid>>,
-    at_crater_query: Query<Entity, With<AtCrater>>,
+    at_crater_model_query: Query<Entity, With<AtCrater>>,
     mut commands: Commands,
+    model_view_entity_map: Res<ModelViewEntityMap>,
 ) {
     if crater_query.get_single().is_ok() {
-        for at_crater_entity in at_crater_query.iter() {
-            commands
-                .entity(at_crater_entity)
-                .insert(Visibility::Visible);
+        for at_crater_model_entity in at_crater_model_query.iter() {
+            if let Some(at_crater_view_entity) =
+                model_view_entity_map.0.get(&at_crater_model_entity)
+            {
+                commands
+                    .entity(*at_crater_view_entity)
+                    .insert(Visibility::Visible);
+            }
         }
     }
 }
@@ -37,14 +43,21 @@ pub fn on_added_crater_visible_grid(
 pub fn on_crater_removed_visible_grid(
     mut removed: RemovedComponents<VisibleGrid>,
     crater_query: Query<&Crater>,
-    at_crater_query: Query<Entity, With<AtCrater>>,
+    at_crater_model_query: Query<Entity, With<AtCrater>>,
     mut commands: Commands,
+    model_view_entity_map: Res<ModelViewEntityMap>,
 ) {
     for entity in &mut removed.read() {
         // If Crater was the one who had VisibleGrid removed
         if let Ok(_) = crater_query.get(entity) {
-            for at_crater_entity in at_crater_query.iter() {
-                commands.entity(at_crater_entity).insert(Visibility::Hidden);
+            for at_crater_model_entity in at_crater_model_query.iter() {
+                if let Some(at_crater_view_entity) =
+                    model_view_entity_map.0.get(&at_crater_model_entity)
+                {
+                    commands
+                        .entity(*at_crater_view_entity)
+                        .insert(Visibility::Hidden);
+                }
             }
         }
     }
--- src/story/nest_simulation/nest/ui.rs
+++ src/story/nest_simulation/nest/ui.rs
@@ -1,20 +1,22 @@
 use bevy::prelude::*;
 
-use crate::story::grid::VisibleGrid;
+use crate::story::{grid::VisibleGrid, nest_simulation::ModelViewEntityMap};
 
 use super::{AtNest, Nest};
 
 // Assume for now that when the simulation loads the user wants to see their nest, but in the future might need to make this more flexible.
 pub fn on_spawn_nest(nest_query: Query<Entity, Added<Nest>>, mut commands: Commands) {
     for nest_entity in nest_query.iter() {
+        // TODO: Stop attaching VisibleGrid to Nest - it's a view concern not a model concern.
         commands.entity(nest_entity).insert(VisibleGrid);
     }
 }
 
 pub fn on_added_at_nest(
     nest_query: Query<&Nest, With<VisibleGrid>>,
-    at_nest_query: Query<Entity, Added<AtNest>>,
+    at_nest_model_query: Query<Entity, Added<AtNest>>,
     mut commands: Commands,
+    model_view_entity_map: Res<ModelViewEntityMap>,
 ) {
     let visibility = if nest_query.get_single().is_ok() {
         Visibility::Visible
@@ -22,19 +24,26 @@ pub fn on_added_at_nest(
         Visibility::Hidden
     };
 
-    for at_nest_entity in at_nest_query.iter() {
-        commands.entity(at_nest_entity).insert(visibility);
+    for at_nest_model_entity in at_nest_model_query.iter() {
+        if let Some(at_nest_view_entity) = model_view_entity_map.0.get(&at_nest_model_entity) {
+            commands.entity(*at_nest_view_entity).insert(visibility);
+        }
     }
 }
 
 pub fn on_added_nest_visible_grid(
     nest_query: Query<&Nest, Added<VisibleGrid>>,
-    at_nest_query: Query<Entity, With<AtNest>>,
+    at_nest_model_query: Query<Entity, With<AtNest>>,
     mut commands: Commands,
+    model_view_entity_map: Res<ModelViewEntityMap>,
 ) {
     if nest_query.get_single().is_ok() {
-        for at_nest_entity in at_nest_query.iter() {
-            commands.entity(at_nest_entity).insert(Visibility::Visible);
+        for at_nest_model_entity in at_nest_model_query.iter() {
+            if let Some(at_nest_view_entity) = model_view_entity_map.0.get(&at_nest_model_entity) {
+                commands
+                    .entity(*at_nest_view_entity)
+                    .insert(Visibility::Visible);
+            }
         }
     }
 }
@@ -42,14 +51,21 @@ pub fn on_added_nest_visible_grid(
 pub fn on_nest_removed_visible_grid(
     mut removed: RemovedComponents<VisibleGrid>,
     nest_query: Query<&Nest>,
-    at_nest_query: Query<Entity, With<AtNest>>,
+    at_nest_model_query: Query<Entity, With<AtNest>>,
     mut commands: Commands,
+    model_view_entity_map: Res<ModelViewEntityMap>,
 ) {
     for entity in &mut removed.read() {
         // If Nest was the one who had VisibleGrid removed
         if let Ok(_) = nest_query.get(entity) {
-            for at_nest_entity in at_nest_query.iter() {
-                commands.entity(at_nest_entity).insert(Visibility::Hidden);
+            for at_nest_model_entity in at_nest_model_query.iter() {
+                if let Some(at_nest_view_entity) =
+                    model_view_entity_map.0.get(&at_nest_model_entity)
+                {
+                    commands
+                        .entity(*at_nest_view_entity)
+                        .insert(Visibility::Hidden);
+                }
             }
         }
     }
