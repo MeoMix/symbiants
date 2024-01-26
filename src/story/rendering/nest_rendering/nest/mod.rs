365718917aaf044fdd5011aed7f5a269adf9454c
Sean Anderson <sanderson@foreground.co>
2024-01-26 01:53:57 +0000

    bug fix: filter using With<Nest> not Added<Nest>
    This was just a dumb mistake, never should've been Added<Nest>

--- src/story/rendering/nest_rendering/nest/mod.rs
+++ src/story/rendering/nest_rendering/nest/mod.rs
@@ -5,7 +5,7 @@ use crate::story::{rendering::common::VisibleGrid, simulation::nest_simulation::
 // TODO: It's weird that I have the concept of `VisibleGrid` in addition to `VisibleGridState`
 // Generally representing the same state in two different ways is a great way to introduce bugs.
 pub fn mark_nest_visible(
-    nest_query: Query<Entity, Added<Nest>>,
+    nest_query: Query<Entity, With<Nest>>,
     mut visible_grid: ResMut<VisibleGrid>,
 ) {
     visible_grid.0 = Some(nest_query.single());
