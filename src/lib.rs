c4a20f7b5b784a12c0cceefe136d4e1ed70136f3
Sean Anderson <sanderson@foreground.co>
2024-01-28 20:03:38 +0000

    misc cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use bevy::{asset::AssetMetaCheck, prelude::*};
+use bevy::prelude::*;
 use bevy_turborand::prelude::*;
 use rendering::RenderingPlugin;
 use simulation::SimulationPlugin;
@@ -8,30 +8,19 @@ pub struct SymbiantsPlugin;
 
 impl Plugin for SymbiantsPlugin {
     fn build(&self, app: &mut App) {
-        // See https://github.com/bevyengine/bevy/pull/10623 for details.
-        app.insert_resource(AssetMetaCheck::Never);
-
-        // Keep this off to prevent spritesheet bleed at various `projection.scale` levels.
-        // See https://github.com/bevyengine/bevy/issues/1949 for details.
-        app.insert_resource(Msaa::Off);
-
         // Use a shared, common source of randomness so that the simulation is deterministic.
         app.init_resource::<GlobalRng>();
 
-        app.add_plugins(
+        app.add_plugins((
             DefaultPlugins
                 .set(WindowPlugin {
                     primary_window: Some(Window {
                         fit_canvas_to_parent: true,
-                        // NOTE: This isn't supported with Wayland.
-                        // mode: WindowMode::SizedFullscreen,
                         ..default()
                     }),
                     ..default()
                 })
                 .set(ImagePlugin::default_nearest()),
-        )
-        .add_plugins((
             RngPlugin::default(),
             UIPlugin,
             SimulationPlugin,
