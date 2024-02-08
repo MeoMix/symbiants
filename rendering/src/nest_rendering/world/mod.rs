7c67b63461a65b2eb49e7a3a55424bfb7f61ab7b
Tomas Seniūnas <>
2024-02-08 16:56:48 +0900

    Restructuring tile rendering for shader use

--- rendering/src/nest_rendering/world/mod.rs
+++ rendering/src/nest_rendering/world/mod.rs
@@ -1,5 +1,5 @@
 pub mod world_map;
-pub use self::world_map::{initialize_world_assets, WorldMap};
+pub use self::world_map::{create_new_world, initialize_tile_worlds, WorldMap};
 
 use bevy::{prelude::*, sprite::Material2dPlugin};
 
@@ -9,6 +9,8 @@ pub struct WorldViewPlugin;
 impl Plugin for WorldViewPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(Material2dPlugin::<WorldMap>::default());
-        app.add_systems(Update, initialize_world_assets);
+        app.add_systems(Update, (create_new_world, initialize_tile_worlds).chain());
     }
+
+    
 }
--- rendering/src/nest_rendering/world/world_map.rs
+++ rendering/src/nest_rendering/world/world_map.rs
@@ -1,18 +1,47 @@
-use std::panic::AssertUnwindSafe;
+use bevy::log::debug;
 
+use bevy::render;
 use bevy::{
     prelude::*,
-    render::render_resource::{AsBindGroup, ShaderRef},
+    render::render_resource::{AsBindGroup, ShaderRef, ShaderType},
     sprite::Material2d,
 };
+
+#[derive(ShaderType, Clone, Debug, Reflect, AsBindGroup)]
+pub struct MapDataUniform {
+    pub map_size: UVec2,
+    pub tile_size: Vec2,
+    pub atlas_size: Vec2,
+    pub transform_local: Mat3,
+    pub transform_world: Mat3,
+    pub size_world: Vec2,
+}
+
+impl Default for MapDataUniform {
+    fn default() -> Self {
+        Self {
+            map_size: default(),
+            tile_size: default(),
+            atlas_size: default(),
+            transform_local: Mat3::IDENTITY,
+            transform_world: default(),
+            size_world: default(),
+        }
+    }
+}
 
 #[derive(Component, Asset, Default, Debug, Clone, Reflect, AsBindGroup)]
 #[reflect(Component)]
 pub struct WorldMap {
-    pub world_size: UVec2,
-    pub tile_size: Vec2,
-    pub atlas_size: Vec2,
-    pub tile_atlas: Handle<TextureAtlas>,
+    #[uniform(0)]
+    pub map_data: MapDataUniform,
+
+    #[storage(32)]
+    pub tile_map: Vec<u32>,
+
+    #[texture(33)]
+    #[sampler(34)]
+    pub atlas: Handle<Image>,
 }
 
 impl Material2d for WorldMap {
@@ -22,33 +51,50 @@ impl Material2d for WorldMap {
 }
 
 impl WorldMap {
-    pub fn new(
-        world_size: UVec2,
-        tile_size: Vec2,
-        atlas_size: Vec2,
-        tile_atlas: Handle<TextureAtlas>,
-    ) -> Self {
-        Self {
-            world_size,
-            tile_size,
-            atlas_size,
-            tile_atlas,
-        }
+    pub fn update(&mut self, images: &Assets<Image>) -> bool {
+        let atlas = match images.get(&self.atlas) {
+            Some(atlas) => atlas,
+            None => return false,
+        };
+
+        self.map_data.atlas_size = atlas.size().as_vec2();
+        true
     }
 }
 
-pub fn initialize_world_assets(
-    map: ResMut<Assets<WorldMap>>,
-    map_handles: Query<&Handle<WorldMap>>,
-    mut images: ResMut<Assets<Image>>,
-    mut ev_asset: EventReader<AssetEvent<Image>>,
+// TODO: This probably needs to stay here, while the rest of world handling
+// can be moved to the parent module so it can be re-used for crater rendering.
+pub fn create_new_world(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<WorldMap>>,
 ) {
-    for ev in ev_asset.iter() {
-        for handle in map_handles.iter() {
-            let Some(map) = map.get(handle) else {
-                /* World has no texture atlas */
-                continue;
-            };
-        }
+    let world_map = WorldMap {
+        map_data: MapDataUniform {
+            tile_size: Vec2::new(32.0, 32.0),
+            ..Default::default()
+        },
+        tile_map: vec![0; 100],
+        atlas: asset_server.load("textures/element/sprite_sheet.png"),
+    };
+
+    materials.add(world_map);
+}
+
+pub fn initialize_tile_worlds(
+    mut assets: ResMut<Assets<WorldMap>>,
+    worlds: Query<(Entity, &Handle<WorldMap>)>,
+    images: Res<Assets<Image>>,
+) {
+    debug!(target: "world", "Initializing tile worlds");
+    for (entity, handle) in worlds.iter() {
+        let Some(map) = assets.get_mut(handle) else {
+            warn!(target: "world", "WorldMap not loaded yet: {:?}", handle);
+            continue;
+            // Not loaded yet, probably. Need to handle.
+        };
+
+        map.update(images.as_ref());
+        warn!(target: "world", "WorldMap updated: {:?}", map);
     }
 }
